2506be291d8e4140cda9fd27d307e4953d62aa9f
Jx123408 <jxjin.work@outlook.com>
2025-03-15 16:07:12 +0900

    config.js を更新

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    username: "jjx",
-    interstellar: "1234",
+    jjx: "master",
+    test: "1234",
+    R↑: "taiko",
+    C: "the-music1",
   },
 };
 
